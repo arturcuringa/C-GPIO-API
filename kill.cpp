007973c47bdc1ff1fec3b9c21adb644a13bbe217
Artur Curinga <arturmcuringa@gmail.com>
2017-03-24 00:53:38 -0300

    correct getProcessCPUusage

--- kill.cpp
+++ kill.cpp
@@ -18,17 +18,22 @@ bool ProcessEntity(struct dirent* entity){
     if(entity->d_type == DT_DIR){
         if(entity->d_name[0] != '.'){
             if(is_number(std::string(entity->d_name)))
-            	return stoi(std::string(entity->d_name)) > 10000 ;
+            	return stoi(std::string(entity->d_name)) > 100 ;
         }
     }return false;
+}
+double abs(double x1)
+{
+	if(x1 < 0)
+		return -1 * x1;
+	return x1;
 }
 
 /*------------------------------*/
 struct Process
 {
 	std::string pid;
-	double info;
-	std::pair<double,double> cpu;
+	int info;
 	double total;
 };
 
@@ -36,7 +41,7 @@ bool comp(Process a, Process b){
 	return a.total > b.total;
 }
 
-std::pair<double,double> getCPUtime()
+std::pair<int,int> getCPUtime()
 {
 		std::fstream fs;
 		fs.open("/proc/stat" ,std::fstream::in);
@@ -46,15 +51,15 @@ std::pair<double,double> getCPUtime()
 		ss << r;
 		std::string name;
 		ss >> name;
-		double user, nice, system, idle;
+		int user, nice, system, idle;
 		ss >> user >> nice >> system >> idle ;
-		std::pair<double,double> x(user + nice + system,  user + nice + system + idle);
+		std::pair<int,int> x(user + nice + system,  user + nice + system + idle);
 		fs.close();
 		return x;
 }
 
-double diffCPUtime(std::pair<double,double>t1 ,std::pair<double,double> t2 ){
-	return (((t2.first - t1.first) / ( t2.second - t1.second)));
+int diffCPUtime(std::pair<int,int>t1 ,std::pair<int,int> t2 ){
+	return  ( t2.second - t1.second);
 }
 
 int getProcessCPUusage(std::string pid)
@@ -67,8 +72,9 @@ int getProcessCPUusage(std::string pid)
 	
 	ss << input;
 	
-	int utime, stime;
+	int utime = 0, stime = 0;
 	for(int i=0; ss>>input; ++i){
+		std::cout << "!!!!" << input << "!!!";
 		if(i>12){
 			std::stringstream ss2(input);
 			if(i==13){
@@ -79,6 +85,7 @@ int getProcessCPUusage(std::string pid)
 			}
 		}
 	}
+	std::cout<<"\n";
 	fs.close();
 	std::cout<<utime << "   "<<stime<<std::endl;
 	return (utime + stime);;
@@ -92,27 +99,31 @@ std::string biggestCPUusage(){
 	DIR *dp;
 
 	Process p;
+	std::pair<double, double> t1 = getCPUtime();
 
 	if((dp = opendir(dirToOpen.c_str())) != NULL){
 		while((d = readdir(dp)) != NULL){
 			if(ProcessEntity(d)){	
 				p.pid=std::string(d->d_name);
-				p.info=getProcessCPUusage(std::string(d->d_name));
-				p.cpu=getCPUtime();
-
+				p.info = getProcessCPUusage(std::string(d->d_name));
 				processes.push_back(p);
+
 			}
 		}
 	}else{
 		std::cout<<"ERROR /PROC"<<std::endl;
 		return "-1";
 	}
+	sleep(1);
 
+	std::pair<double, double> t2 = getCPUtime();	
+	
 	if((dp = opendir(dirToOpen.c_str())) != NULL){
 		for(int i=0; i<processes.size(); ++i){
 			if((d = readdir(dp)) != NULL){
-				processes[i].total= 100*(std::abs(getProcessCPUusage(std::string(d->d_name)) - processes[i].info) / 
-									std::abs(diffCPUtime(p.cpu,getCPUtime())));
+
+				processes[i].total= 100*( std::abs(getProcessCPUusage(std::string(d->d_name)) - processes[i].info) / 
+									std::abs( diffCPUtime(t1,t2) ) );
 				
 				std::cout<<processes[i].pid<<":  "<<processes[i].total<<std::endl;
 			}else{
@@ -121,20 +132,22 @@ std::string biggestCPUusage(){
 		}
 	}
 	std::sort(processes.begin(),processes.end(),comp);
-	std::cout<<"----------------------"<<std::endl;
-	for(int i=0; i<processes.size(); ++i){
-		std::cout<<processes[i].pid<<":  "<<processes[i].total<<std::endl;
-	}
-	std::cout<<"----------------------"<<std::endl;
-	std::cout<<"----------------------"<<std::endl;
-	std::cout<<"----------------------"<<std::endl;
-	std::cout<<processes[0].pid<<":  "<<processes[0].total<<std::endl;
+	// std::cout<<"----------------------"<<std::endl;
+	// for(int i=0; i<processes.size(); ++i){
+	// 	if(processes[i].pid ==  "7187")
+	// 		std::cout << "CARAI " << processes[i].total <<std::endl; 
+	// 	std::cout<<processes[i].pid<<":  "<<processes[i].total<<std::endl;
+	// }
+	// // std::cout<<"----------------------"<<std::endl;
+	// std::cout<<"----------------------"<<std::endl;
+	// std::cout<<"----------------------"<<std::endl;
+	// std::cout<<processes[0].pid<<":  "<<processes[0].total<<std::endl;
 	return (processes.empty() ? "-1": processes[0].pid);
 }
 
 int main(void){
 	std::string p= biggestCPUusage();
 	std::cout<< "KILLING PROCESS: "<<stoi(p)<<std::endl;
-	//kill(stoi(p),SIGQUIT);
+	kill(stoi(p),SIGQUIT);
 	return 0;
 }
